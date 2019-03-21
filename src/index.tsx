cc1c3d59d6e54f9658011318eb4629333430d032
Himenon <6715229+Himenon@users.noreply.github.com>
2019-03-22 01:51:00 +0900

    feat(css-module): update webpack setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { HelloWorld } from "./HelloWorld";
-import "./index.css";
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
